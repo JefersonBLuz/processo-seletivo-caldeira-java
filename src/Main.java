fb53d6f2e3b6ca9eba6bb25416d6da0acd9867dd
JefersonBLuz <jefersonbrluz@gamil.com>
2025-06-22 02:01:46 -0300

    Adicionado Atividade Avaliacao.java e atualizado o `Main`.

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1º Questão, Agendamento, resultado:");
         Atividades.Agendamento.main(args);
+        System.out.println("2º Questão, Avaliação, resultado:");
+        Atividades.Avaliacao.main(args);
     }
 }
